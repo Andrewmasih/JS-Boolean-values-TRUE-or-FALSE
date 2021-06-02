c7c63d1d9a8557de793bddc3cbcde96a3795da04
Andrew Masih <armasih@outlook.com>
2021-06-02 21:31:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/* TRUE and FalSE determines how a program runs, for example: if a number is 0, null or an empty string, then it will be FALSE, if a string is not empty or a number is not equal to 0 it will be considered TRUE.
+
+We can use Boolean function to create a new boolean based on whether or not these values are TRUE or FALSE */
+
+
+// boolean(thing you want to check);
+
+
+// each of these statement will be TRUE
+console.log(Boolean ("apple"));
+console.log(Boolean (2));
+console.log(Boolean (-1.5));
+
+
+// each of these statements wil be FALSE
+console.log(Boolean (""));
+console.log(Boolean (-0));
+console.log(Boolean (null));
